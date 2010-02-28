fb810bc6a37119f8c703b48790cd521696bb0c3f
ngn <jeliazkova.nina@gmail.com>
2010-02-28 13:01:49 +0000

    Setting ARQ sparql syntax as default (to be able to use aggregates) http://jena.sourceforge.net/ARQ/group-by.html

--- src/main/java/org/opentox/service/ontology/OntologyResource.java
+++ src/main/java/org/opentox/service/ontology/OntologyResource.java
@@ -30,6 +30,7 @@ import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.query.Syntax;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.shared.Lock;
@@ -119,8 +120,7 @@ public class OntologyResource<T extends Serializable> extends ServerResource {
 						ontology.enterCriticalSection(Lock.READ) ;
 					
 					try {
-						Query query = QueryFactory.create(queryString);
-						
+						Query query = QueryFactory.create(queryString,null,Syntax.syntaxARQ);
 
 						// Execute the query and obtain results
 						qe = QueryExecutionFactory.create(query,ontology );
