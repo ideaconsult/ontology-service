20905ca56f51a066910b073ab2fd1953c683d23e
ngn <jeliazkova.nina@gmail.com>
2010-10-06 18:29:18 +0000

    refactoring in preparation to replace TDB storage with SDB

---
+++ src/main/java/org/opentox/service/ontology/SDBOntologyResource.java
@@ -0,0 +1,15 @@
+package org.opentox.service.ontology;
+
+import org.restlet.resource.ResourceException;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class SDBOntologyResource extends AbstractOntologyResource {
+
+	@Override
+	protected Model createOntologyModel(boolean init) throws ResourceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
